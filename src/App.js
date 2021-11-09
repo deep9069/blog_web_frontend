eba89b748287c255eb7f35c89f2827250d1dddce
Harshsinghs1058 <harshsinghs1058@gmail.com>
2021-11-09 19:14:23 +0530

    debugged sign in page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignIn from "./components/login/sign_in_page";
+import SignUp from "./components/sign_up/sign_up_page";
 function App() {
   return (
-    <div className="App">
-      Hello world
-    </div>
+    <Router>
+      <Routes >
+        <Route exact path="/sign-up" element={<SignUp />} />
+        <Route exact path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </Router>
   );
 }
 
