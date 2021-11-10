14f970cc680451252c91cd4ff4771fc46ca97263
Harshsinghs1058 <harshsinghs1058@gmail.com>
2021-11-10 09:53:28 +0530

    testing gh-pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { HashRouter, Route, Routes, Link } from "react-router-dom";
+import BlogPage from "./components/blog_page/blog_page";
 import SignIn from "./components/login/sign_in_page";
 import SignUp from "./components/sign_up/sign_up_page";
 function App() {
@@ -9,10 +10,12 @@ function App() {
           <div>
             <Link to="sign-in"> Sign in </Link>
             <Link to="sign-up"> Sign up </Link>
+            <Link to="/blogs"> blog page </Link>
           </div>
         } />
         <Route exact path="/sign-up" element={<SignUp />} />
         <Route exact path="/sign-in" element={<SignIn />} />
+        <Route exact path="/sign-in" element={<BlogPage />} />
       </Routes>
     </HashRouter>
   );
